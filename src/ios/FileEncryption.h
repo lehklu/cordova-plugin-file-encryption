ddceee8ac7e8b058ba2bb9dfde16827a6882bc92
Werner Lechner <werner.lechner.2@gmail.com>
2019-01-07 09:35:43 +0100

    ios: changed include fpr CORDOVA 4.0.0

--- src/ios/FileEncryption.h
+++ src/ios/FileEncryption.h
@@ -1,7 +1,9 @@
 #import <Foundation/Foundation.h>
 #import <Security/SecRandom.h>
 #import <Cordova/CDV.h>
+#ifndef __CORDOVA_4_0_0
 #import <Cordova/NSData+Base64.h>
+#endif
 #import "RNEncryptor.h"
 #import "RNDecryptor.h"
 
