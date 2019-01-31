84b809f4b71b7f08b2a6989f5c5b665d951399cf
Werner Lechner <werner.lechner.2@gmail.com>
2019-01-31 09:36:47 +0100

    iOS: -use path from arguments -return error msg -refactor code

--- src/ios/FileEncryption.m
+++ src/ios/FileEncryption.m
@@ -2,6 +2,14 @@
 
 @implementation FileEncryption
 
+
+NSString *const PREFIX_ERROR = @"ERR: ";
+
+- (NSString *)_asError:(NSString *)msg {
+
+	return [PREFIX_ERROR stringByAppendingString:msg];
+}
+
 /**
  *  encrypt
  *
@@ -9,20 +17,9 @@
  */
 - (void)encrypt:(CDVInvokedUrlCommand *)command {
 
-  CDVPluginResult *pluginResult = nil;
-
-  NSString *path = [self crypto:@"encrypt" command:command];
-
-  if (path != nil) {
-    pluginResult =
-        [CDVPluginResult resultWithStatus:CDVCommandStatus_OK
-                          messageAsString:path];
-  } else {
-    pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
-  }
-
-  [self.commandDelegate sendPluginResult:pluginResult
-                              callbackId:command.callbackId];
+  [self.commandDelegate
+  	sendPluginResult:[self crypto:@"encrypt" command:command]
+  	callbackId:command.callbackId];
 }
 
 /**
@@ -32,20 +29,9 @@
  */
 - (void)decrypt:(CDVInvokedUrlCommand *)command {
 
-  CDVPluginResult *pluginResult = nil;
-
-  NSString *path = [self crypto:@"decrypt" command:command];
-
-  if (path != nil) {
-    pluginResult =
-        [CDVPluginResult resultWithStatus:CDVCommandStatus_OK
-                          messageAsString:path];
-  } else {
-    pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
-  }
-
-  [self.commandDelegate sendPluginResult:pluginResult
-                              callbackId:command.callbackId];
+  [self.commandDelegate
+  	sendPluginResult:[self crypto:@"decrypt" command:command]
+  	callbackId:command.callbackId];
 }
 
 /**
@@ -55,52 +41,73 @@
  *  @param action  Cryptographic operation
  *  @param command Cordova arguments
  *
- *  @return Boolean value representing success or failure
+ *  @return result of operation
  */
-- (NSString*)crypto:(NSString *)action command:(CDVInvokedUrlCommand *)command {
+- (CDVPluginResult*)crypto:(NSString *)action command:(CDVInvokedUrlCommand *)command {
 
   NSData *data = nil;
   NSString *filePath = [command.arguments objectAtIndex:0];
   NSString *password = [command.arguments objectAtIndex:1];
+  BOOL passIsKey = [command.arguments objectAtIndex:2];
   NSString *fileName = [filePath lastPathComponent];
   NSFileManager *fileManager = [NSFileManager defaultManager];
   NSString *documentsPath = [NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES) firstObject];
-  NSString *path = [documentsPath stringByAppendingPathComponent:fileName];
+
+  NSString *path = passIsKey?filePath:[documentsPath stringByAppendingPathComponent:fileName];
   BOOL fileExists = [fileManager fileExistsAtPath:path];
 
-  // if path and password args exist
-  if (path != nil && [path length] > 0 && password != nil &&
-      [password length] > 0) {
 
-    // if file exists
-    if (fileExists) {
+  if(path==nil || [path length]==0 || password == nil || [password length] == 0)
+  	return [CDVPluginResult
+  						resultWithStatus:CDVCommandStatus_ERROR
+  						messageAsString:[self _asError: @"Empty argument"]];
 
-      // get file data
-      NSData *fileData = [NSData dataWithContentsOfFile:path];
+  if(! fileExists)
+  	return [CDVPluginResult
+  						resultWithStatus:CDVCommandStatus_ERROR
+  						messageAsString:[self _asError: @"File not found"]];
 
-      NSError *error;
-      if ([action isEqualToString:@"encrypt"]) {
-        // encrypt data
-        data = [RNEncryptor encryptData:fileData
-                           withSettings:kRNCryptorAES256Settings
-                               password:password
-                                  error:&error];
 
-      } else if ([action isEqualToString:@"decrypt"]) {
-        // decrypt data
-        data = [RNDecryptor decryptData:fileData
-                           withPassword:password
-                                  error:&error];
-      }
+	NSData *fileData = [NSData dataWithContentsOfFile:path];
 
-      // write to generated path
-      [data writeToFile:path atomically:YES];
-    } else {
-      path = nil;
-    }
+  NSError *error;
+  if ([action isEqualToString:@"encrypt"])
+  {
+    data = [RNEncryptor
+    					encryptData:fileData
+              withSettings:kRNCryptorAES256Settings
+              password:password
+              error:&error];
+
+	}
+	else if ([action isEqualToString:@"decrypt"])
+	{
+    data = [RNDecryptor
+    					decryptData:fileData
+              withPassword:password
+              error:&error];
+	}
+	else
+	{
+		return [CDVPluginResult
+  						resultWithStatus:CDVCommandStatus_ERROR
+  						messageAsString:[self _asError: @"Action not 'encrypt' or 'decrypt'"]];
+	}
+
+
+  if(error != nil)
+  {
+		return [CDVPluginResult
+  						resultWithStatus:CDVCommandStatus_ERROR
+  						messageAsString:[self _asError: [error localizedDescription]]];
   }
 
-  return path;
+
+
+	[data writeToFile:path atomically:YES];
+	return [CDVPluginResult
+						resultWithStatus:CDVCommandStatus_OK
+						messageAsString:path];
 }
 
 @end
